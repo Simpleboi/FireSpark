6972011a7ae2a2e458061be21516c4389b10b9c1
Biggie Cheese <Jv3studios@gmail.com>
2025-03-21 20:12:17 -0500

    fixed bundle error

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-// export { Button } from "./components/Button/Button.react";
+// To export all the components
+export { Button } from "./components/Button/Button.react";
 // export type { ButtonProps } from "./components/Button/Button.react";
-
-// export {Input } from "./components/Input/Input.react";
-// export type { InputProps } from "./components/Input/Input.react";
-
-// export { ProgressBar } from "./components/ProgressBar/ProgressBar.react";
-// export type { ProgressBarProps } from "./components/ProgressBar/ProgressBar.react";
 
---
+++ src/tests/example.js
@@ -0,0 +1,7 @@
+import { Button } from "../components/Button/Button.react"
+
+const head = document.getElementById("header");
+
+if (head) {
+    head.style.border = "1px solid black";
+}
