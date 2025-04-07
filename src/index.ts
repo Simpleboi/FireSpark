03a56a4a680bf33104425bd96dc3533091a1aa17
Biggie Cheese <Jv3studios@gmail.com>
2025-04-07 11:04:56 -0500

    more component tests

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 // To export all the components
+import "firespark/dist/bundle.css";
+
 export { Button } from "./components/Button/Button.react";
-// export type { ButtonProps } from "./components/Button/Button.react";
+
+export type { ButtonProps } from "./components/Button/Button.react";
 
